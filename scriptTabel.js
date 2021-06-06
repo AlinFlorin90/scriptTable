98e4b64c1f572fb7c6d8f3690e693766a3e76373
Albastroiu Alin <albastroiu.alin.florin@gmail.com>
2021-06-06 17:44:40 +0200

    firstCommitWithErrors

---
+++ scriptTabel.js
@@ -0,0 +1,22 @@
+fetch("https://jsonplaceholder.typicode.com/users",{
+    method: "get",
+    headers: {
+        Accept: "application/json, plain/text, */*",
+        "Content-Type": "application/json",
+    }
+})
+.then((response) => response.json())
+  .then((data) => {
+    let i = 0;
+    console.log(data);
+    data.forEach((person) => {
+      document.querySelector("#a" + i).innerText = person.id;
+      i++;
+      document.querySelector("#a" + i).innerText = person.name;
+      i++;
+      document.querySelector("#a" + i).innerText = person.address.city;
+      i++;
+      document.querySelector("#a" + i).innerText = person.phone;
+      i++;
+    });
+  });
