4b5bb4838235e0361e3131e34a0c57c6406ba071
Albastroiu Alin <albastroiu.alin.florin@gmail.com>
2021-06-09 15:05:13 +0200

    last commit-done!

--- scriptTabel.js
+++ scriptTabel.js
@@ -9,14 +9,21 @@ fetch("https://jsonplaceholder.typicode.com/users",{
   .then((data) => {
     let i = 0;
     console.log(data);
-    data.forEach((person) => {
-      document.querySelector("#a" + i).innerText = person.id;
-      i++;
-      document.querySelector("#a" + i).innerText = person.name;
-      i++;
-      document.querySelector("#a" + i).innerText = person.address.city;
-      i++;
-      document.querySelector("#a" + i).innerText = person.phone;
-      i++;
-    });
+    
+     data.forEach((person) => {
+        
+        if(i<=12){
+            celula= document.createElement("td");
+            celula.classList.add("a"+ i);
+            document.querySelector(".a" + i).innerText = person.id;
+            i++;
+            document.querySelector(".a" + i).innerText = person.name;
+            i++;
+            document.querySelector(".a" + i).innerText = person.address.city;
+            i++;
+            document.querySelector(".a" + i).innerText = person.phone;
+            i++;
+            }    
+        });
   });
+
